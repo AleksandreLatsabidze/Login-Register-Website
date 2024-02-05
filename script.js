523ff81424516453ae3b96c9e19eb7851e24ca5c
aleksandrelcb <121313546+AleksandreLatsabidze@users.noreply.github.com>
2024-02-05 16:35:06 +0400

    I added a new feature

--- script.js
+++ script.js
@@ -17,3 +17,24 @@ showRegistr.addEventListener("click", () => {
 hideRegistr.addEventListener("click", () => {
   wrapper.classList.remove("active-popup");
 });
+
+let inputUp = document.getElementById("inputUp");
+let emailUp = document.getElementById("emailUp");
+let inputBox = document.getElementById("input-box");
+
+var email = document.getElementById("inputUp").value;
+var emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+inputUp.addEventListener("keydown", function () {
+  if (inputUp.value != "") {
+    emailUp.classList.add("doneUp");
+  } else {
+    emailUp.classList.remove("doneUp");
+    // inputBox.classList.remove("error");
+  }
+  if (!emailRegex.test(email)) {
+    inputBox.classList.add("error");
+  } else {
+    inputBox.classList.remove("error");
+  }
+});
